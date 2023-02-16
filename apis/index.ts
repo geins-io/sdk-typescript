e11deb73a1ec862f0dfbe0ee00385dc90b0231d0
arvidsson-geins <arvidsson@geins.io>
2023-02-16 17:30:31 +0100

    init

---
+++ apis/index.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BrandApi';
+export * from './CategoryApi';
+export * from './MarketApi';
+export * from './OrderApi';
+export * from './PageAreaApi';
+export * from './PaymentApi';
+export * from './PriceListApi';
+export * from './ProductApi';
+export * from './ProductParameterApi';
+export * from './ShippingApi';
+export * from './SupplierApi';
+export * from './UserApi';
+export * from './VariantApi';
